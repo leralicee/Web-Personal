01245cbac4f2ed28a9a4514ff2bfaf4751e2d1d3
Paula <alu.paula.romero@mataro.epiaedu.cat>
2025-09-30 13:45:31 +0200

    fet readme

---
+++ readme.mk
@@ -0,0 +1,18 @@
+# Lloc web personal
+
+Aquest és el meu lloc web personal fet com a treball escolar.  
+
+## Descripció
+És una pàgina web que mostra informació personal amb una secció de text i una imatge. També inclou un botó que pot descarregar un fitxer.  
+
+La pàgina està feta amb HTML i CSS i utilitza una font de Google Fonts (Poppins).  
+
+## Funcions
+- Navbar amb enllaços.
+- Secció "Home" amb text i imatge.
+- Botó estilitzat per descarregar fitxers.
+- Animació simple del text amb efecte de "typing".
+
+---
+
+Fi.
